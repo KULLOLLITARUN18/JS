5350bbb437ba53ea62711fa9901e14700d290f47
KULLOLLITARUN18 <kullollitharun@gmail.com>
2024-09-04 07:20:28 +0000

    container config done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+/*console.log(3 
+    +
+     3) // code readability should be high*/
+
+console.log(3+3)
+
+
+console.log("Tarun")
+
+let name="Tarun"
+let age=22
+let isLoggedIn= false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof age);
+console.log(typeof "Tarun"); // object
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
